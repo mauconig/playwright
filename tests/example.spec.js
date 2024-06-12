266b036ed91af94dd76c6d8205a8e9d3f5088134
mauricio-conigliaro-itti <mauricio.conigliaro@itti.digital>
2024-06-12 18:12:27 -0400

    New playwright test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,5 +3,4 @@ const { test, expect } = require("@playwright/test");
 test("First Test", async ({ page }) => {
   // Write testing code here
   await page.goto("https://playwright.dev/");
-  await page.pause();
 });
